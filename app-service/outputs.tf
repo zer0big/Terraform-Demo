5bf7ecde64cdd3925c5c48a8551108af4a124074
zerobig <zerobig.kim@gmail.com>
2019-11-03 17:05:25 +0900

    Updatedoutputs.tf

--- app-service/outputs.tf
+++ app-service/outputs.tf
@@ -1,7 +1,7 @@
 output "app_service_name" {
-  value = "${azurerm_app_service.zerobigtest.app_service_name}"
+  value = "${azurerm_app_service.zerobigtest.name}"
 }
 
 output "app_service_default_hostname" {
-  value = "https://${azurerm_app_service.zerobigtest.app_service_default_hostname}"
+  value = "https://${azurerm_app_service.zerobigtest.default_site_hostname}"
 }
