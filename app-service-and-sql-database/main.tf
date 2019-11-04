a2ea78e084cd78ebd49c1a40a86282121b3df4a4
zerobig <zerobig.kim@gmail.com>
2019-11-04 09:00:35 +0900

    Update main.tf

--- app-service-and-sql-database/main.tf
+++ app-service-and-sql-database/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" "zerobigtest" {
+resource "azurerm_app_service" "test" {
   name                = "${var.app-service-name}"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
